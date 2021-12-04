8e39db1a4f1ef0153377515832b3d14d2e38003c
udertaker-ilya <ilya.lisovskii@gmail.com>
2021-12-04 16:21:04 +0300

    add Core

---
+++ Core/kamtoday/KamToDaySettings.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Parser.Core.Habra
+{
+    class KamToDaySettings : IParserSettings
+    {
+        public KamToDaySettings(int start, int end)
+        {
+            StartPoint = start;
+            EndPoint = end;
+        }
+
+        public string BaseUrl { get; set; } = "https://kamtoday.ru/news/ecologics/";
+
+        public string Prefix { get; set; } = "?PAGEN_1={CurrentId}";
+
+        public int StartPoint { get; set; }
+
+        public int EndPoint { get; set; }
+    }
+}
---
+++ Core/kamtoday/KamtoDayParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Html;
+using AngleSharp.Html.Dom;
+
+namespace Parser.Core.Habra
+{
+    class KamtoDayParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            var list  = new List<string>();
+            var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("name"));
+
+            foreach(var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
