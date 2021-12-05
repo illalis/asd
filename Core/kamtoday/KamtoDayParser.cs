b0f0bff13ea27a3e0ef1a26608c4b40291037dda
udertaker-ilya <ilya.lisovskii@gmail.com>
2021-12-05 11:06:26 +0300

    aaaa

--- Core/kamtoday/KamtoDayParser.cs
+++ Core/kamtoday/KamtoDayParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +11,51 @@ namespace Parser.Core.Habra
 {
     class KamtoDayParser : IParser<string[]>
     {
+        List<string> PermittedList = new List<string>() {
+        "вулкан",
+
+
+        };
+        public static List<string> RemoveDuplicates(List<string> list1)
+        {
+          
+            return list1.Distinct().ToList();
+        }
+
+
         public string[] Parse(IHtmlDocument document)
         {
+
+            
+
             var list  = new List<string>();
-            var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null && item.ClassName.Contains("name"));
+            var items = document.QuerySelectorAll("a, span").Where(item => item.ClassName != null && (item.ClassName.Contains("name") || item.ClassName.Contains("date darkgray")));
+           // int i = 0;
 
             foreach(var item in items)
             {
+               
                 list.Add(item.TextContent);
+                
+                list.Remove("#Экология");
+
+                
+                }
+            
+            string filepath = "D:\\AllData.txt";
+            StreamWriter f = new StreamWriter("D:\\AllData.txt", true);
+            for (int i = 0; i < list.Count ; i++)
+            {
+                f.WriteLine(list[i]);
+
             }
+
+            f.Close();
+            //  List<string> list1 = list.Distinct().ToList();
+           
+
+       
+
 
             return list.ToArray();
         }
--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -35,6 +35,9 @@
             this.label2 = new System.Windows.Forms.Label();
             this.ButtonStart = new System.Windows.Forms.Button();
             this.ButtonAbort = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.NumericStart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.NumericEnd)).BeginInit();
             this.SuspendLayout();
@@ -47,9 +50,9 @@
             this.ListTitles.FormattingEnabled = true;
             this.ListTitles.ItemHeight = 16;
             this.ListTitles.Location = new System.Drawing.Point(16, 15);
-            this.ListTitles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ListTitles.Margin = new System.Windows.Forms.Padding(4);
             this.ListTitles.Name = "ListTitles";
-            this.ListTitles.Size = new System.Drawing.Size(292, 356);
+            this.ListTitles.Size = new System.Drawing.Size(292, 148);
             this.ListTitles.TabIndex = 0;
             // 
             // label1
@@ -67,7 +70,7 @@
             // 
             this.NumericStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.NumericStart.Location = new System.Drawing.Point(321, 34);
-            this.NumericStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NumericStart.Margin = new System.Windows.Forms.Padding(4);
             this.NumericStart.Minimum = new decimal(new int[] {
             1,
             0,
@@ -86,7 +89,7 @@
             // 
             this.NumericEnd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.NumericEnd.Location = new System.Drawing.Point(321, 86);
-            this.NumericEnd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NumericEnd.Margin = new System.Windows.Forms.Padding(4);
             this.NumericEnd.Minimum = new decimal(new int[] {
             1,
             0,
@@ -116,19 +119,19 @@
             // 
             this.ButtonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.ButtonStart.Location = new System.Drawing.Point(321, 134);
-            this.ButtonStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ButtonStart.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonStart.Name = "ButtonStart";
             this.ButtonStart.Size = new System.Drawing.Size(132, 28);
             this.ButtonStart.TabIndex = 5;
             this.ButtonStart.Text = "Start";
             this.ButtonStart.UseVisualStyleBackColor = true;
-            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_ClickAsync);
             // 
             // ButtonAbort
             // 
             this.ButtonAbort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.ButtonAbort.Location = new System.Drawing.Point(321, 170);
-            this.ButtonAbort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ButtonAbort.Margin = new System.Windows.Forms.Padding(4);
             this.ButtonAbort.Name = "ButtonAbort";
             this.ButtonAbort.Size = new System.Drawing.Size(132, 28);
             this.ButtonAbort.TabIndex = 6;
@@ -136,11 +139,43 @@
             this.ButtonAbort.UseVisualStyleBackColor = true;
             this.ButtonAbort.Click += new System.EventHandler(this.ButtonAbort_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 203);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(292, 164);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(321, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(469, 383);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.ButtonAbort);
             this.Controls.Add(this.ButtonStart);
             this.Controls.Add(this.NumericEnd);
@@ -148,7 +183,7 @@
             this.Controls.Add(this.NumericStart);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ListTitles);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormMain";
             this.Text = "KamTodayParser";
             this.Load += new System.EventHandler(this.FormMain_Load);
@@ -168,6 +203,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button ButtonStart;
         private System.Windows.Forms.Button ButtonAbort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
